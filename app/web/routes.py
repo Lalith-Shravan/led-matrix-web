f9066fd14e34149137b7043ed5a950d2161cbb59
Lalith-Shravan <lalithg@outlook.com>
2025-06-30 22:54:55 -0700

    Added bar chart functionality

--- app/web/routes.py
+++ app/web/routes.py
@@ -3,6 +3,13 @@ from flask import render_template, request, redirect, jsonify, current_app
 from werkzeug.utils import secure_filename
 from . import web
 
+import uuid
+
+import json
+import matplotlib.pyplot as plt
+
+from app.display.rich_text import RichText
+
 from app.utils.html_parser import html_to_rich_text
 from app.utils.get_file_extension import getFileExtension
 
@@ -39,3 +46,41 @@ def queue_image():
         return redirect("/")
     else:
         return "Invalid file type"
+
+@web.route("/send-graph", methods=["POST"])
+def queue_graph():
+    try:
+        data = request.get_json()
+        message = data['message']
+        
+        message = json.loads(message)
+
+        data = message.get("data")
+        x_values = data.keys()
+        y_values = []
+
+        for x_value in x_values:
+            y_values.append(data.get(x_value))
+
+        # TODO: Make figsize fit
+        plt.figure(figsize=(2, 1))
+        
+        plt.bar(x_values, y_values)
+
+        plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+        plt.axis('off')
+        plt.margins(0)
+
+        name = f"/{uuid.uuid4()}.png"
+
+        filePath = current_app.config['UPLOAD_FOLDER'] + name
+        
+        plt.savefig(filePath)
+
+        
+        current_app.queue.put(filePath)
+        
+        return jsonify({"success": True, "message": "Message added to the queue"}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)}), 500
